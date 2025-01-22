c53b711f502d889fd6779befdc3b755e8743ff73
Reima Riihimäki <reimarii@oamk.fi>
2025-01-22 11:05:45 +0200

    Solution

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import InputSpinner from 'react-native-input-spinner';
 import { PaperProvider, RadioButton } from 'react-native-paper';
-import { Dropdown } from 'react-native-paper-dropdown';
+import { Dropdown, DropdownInput } from 'react-native-paper-dropdown';
 
 
 const TRAIN_SPLIT = {
@@ -15,11 +15,58 @@ const TRAIN_SPLIT = {
 
 export default function App() {
 
-  
+  const [splitday, setSplitday] = useState('Day1');
+  const [exercise, setExercise] = useState(TRAIN_SPLIT.Day1[0]);
+  const [repetitions, setRepetitions] = useState(0);
+
+  const options = TRAIN_SPLIT[splitday].map(e => ({ label: e, value: e }));
+
+  function updateSplitDay(day) {
+    //Update the first exercise for the dropdown from the split day array
+    setExercise(TRAIN_SPLIT[day][0]);
+    //Update the splitday state
+    setSplitday(day);
+  }
+
   return (
     <PaperProvider>
-    
+      <View style={styles.container}>
+        <RadioButton.Group value={splitday} onValueChange={updateSplitDay}>
+          <View style={styles.radioButtonView}>
+            <RadioButtonView label={'Day1'} />
+            <RadioButtonView label={'Day2'} />
+            <RadioButtonView label={'Day3'} />
+          </View>
+        </RadioButton.Group>
+        <Dropdown
+          options={options}
+          value={exercise}
+          onSelect={setExercise}
+          mode='outlined'
+        />
+        <View style={styles.spinnerView}>
+          <Text>Repetition:</Text>
+          <View>
+            <InputSpinner
+              value={repetitions}
+              onChange={setRepetitions}
+              skin='modern'
+            />
+          </View>
+        </View>
+        <Text style={styles.result}>{exercise} x {repetitions}</Text>
+
+      </View>
     </PaperProvider>
+  );
+}
+
+function RadioButtonView({ label }) {
+  return (
+    <View style={styles.radioButtonView}>
+      <Text>{label}</Text>
+      <RadioButton value={label} />
+    </View>
   );
 }
 
@@ -27,7 +74,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    padding: 20,
+    gap: 10
+  },
+  radioButtonView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  spinnerView:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    gap: 10
+  },
+  result:{
+    fontSize: 22,
+    fontWeight: 'bold'
   }
 });
